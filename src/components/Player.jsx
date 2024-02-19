0cf34d6036692bb51fb36d1bc231e7f40a5c13d6
Atul1901 <kumar.atul.20299@gmail.com>
2024-02-19 16:36:37 +0530

    created the player and there symbol

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [currentName, setName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleSelect = () => {
+    setIsEditing((editing) => !isEditing);
+  };
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  let playerName = <span className="player-name">{currentName}</span>;
+  let buttonCaption = "Edit";
+
+  if (isEditing) {
+    playerName = (
+      <input type="text" required value={currentName} onChange={handleName} />
+    );
+    buttonCaption = "Save";
+  }
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-name">{symbol}</span>
+      </span>
+      <button onClick={handleSelect}>{buttonCaption}</button>
+    </li>
+  );
+}
